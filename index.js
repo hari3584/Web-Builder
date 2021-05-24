86c96b98a4a3ab392bce39294fdaeb5e4b60f336
Sreehari N <44242559+hari3584@users.noreply.github.com>
2021-05-24 17:52:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(<div><ul>
+                        <li>REACT</li>
+                        <li>Python</li></ul></div>, document.getElementById('root'))
